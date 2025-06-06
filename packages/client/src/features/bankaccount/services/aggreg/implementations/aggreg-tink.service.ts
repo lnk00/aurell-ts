d1b62869d537cef4cbcca2d70eaf6f5b1f9fd105
Damien Dumontet <damien.dumontet@protonmail.com>
2025-06-06 19:14:43 -0300

    redirect to tinkLink url

--- packages/client/src/features/bankaccount/services/aggreg/implementations/aggreg-tink.service.ts
+++ packages/client/src/features/bankaccount/services/aggreg/implementations/aggreg-tink.service.ts
@@ -5,7 +5,19 @@ import { getLinkCode } from '@/features/bankaccount/api/get-link-code.api';
 @injectable()
 export class AggregTinkService implements AggregService {
 	async openAggregator() {
-		const code = await getLinkCode();
-		console.log(code);
+		const tinkLinkUrl = new URL('https://link.tink.com/1.0/credentials/add');
+
+		const { code } = await getLinkCode();
+		tinkLinkUrl.searchParams.append(
+			'client_id',
+			import.meta.env.VITE_TINK_CLIENT_ID as string,
+		);
+		tinkLinkUrl.searchParams.append(
+			'redirect_uri',
+			`${window.location.origin}/bankaccount/aggreg/callback`,
+		);
+
+		tinkLinkUrl.searchParams.append('authorization_code', code);
+		window.location.href = tinkLinkUrl.toString();
 	}
 }
