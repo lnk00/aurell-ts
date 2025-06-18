5e4d4ea11312449d9ba64ca67c0246b1431a90ae
Damien Dumontet <damien.dumontet@protonmail.com>
2025-06-18 09:58:23 -0300

    Fix biomejs

--- packages/server/src/features/bankaccount/handlers/index.ts
+++ packages/server/src/features/bankaccount/handlers/index.ts
@@ -1,23 +1,20 @@
-import { drizzle } from 'drizzle-orm/d1';
-import { Hono } from 'hono';
-import { validator } from 'hono/validator';
-import z from 'zod/v4';
-import { bankConnections } from '../../../libs/db/schemas/bank-connection.schema';
-import { getService } from '../../../libs/ioc.lib';
-import { Validate } from '../../../libs/validator.lib';
-import type { HonoContextType } from '../../../types/context.type';
-import { guardMiddleware } from '../../auth/middlewares/guard.middleware';
-import { DatabaseError } from '../../../types/errors.type';
+import { Hono } from "hono";
+import { validator } from "hono/validator";
+import z from "zod/v4";
+import { getService } from "../../../libs/ioc.lib";
+import { Validate } from "../../../libs/validator.lib";
+import type { HonoContextType } from "../../../types/context.type";
+import { guardMiddleware } from "../../auth/middlewares/guard.middleware";
 
 const schema = z.object({
-	bankAccountId: z.string('bankAccountId is required in the request body'),
+	bankAccountId: z.string("bankAccountId is required in the request body"),
 });
 
 const openbankingHandlers = new Hono<HonoContextType>()
-	.use('*', guardMiddleware)
-	.get('/link/code', async (c) => {
-		const userId = c.get('userId');
-		const obCoreService = getService('obcore');
+	.use("*", guardMiddleware)
+	.get("/link/code", async (c) => {
+		const userId = c.get("userId");
+		const obCoreService = getService("obcore");
 
 		const { linkCode } = await obCoreService.getLinkCode(userId as string);
 
@@ -29,33 +26,17 @@ const openbankingHandlers = new Hono<HonoContextType>()
 		);
 	})
 	.post(
-		'/link/aggreg',
-		validator('form', (value) => Validate(value, schema)),
+		"/link/aggreg",
+		validator("form", (value) => Validate(value, schema)),
 		async (c) => {
-			const userId = c.get('userId');
-			const { bankAccountId } = c.req.valid('form');
-
-			try {
-				const db = drizzle(c.env.DB);
-				await db
-					.insert(bankConnections)
-					.values({
-						id: bankAccountId,
-						userId: userId as string,
-					})
-					.onConflictDoNothing();
-			} catch (e) {
-				throw new DatabaseError('Could not insert the bank connectio');
-			}
-
 			return c.json({
 				success: true,
 			});
 		},
 	)
-	.get('/accounts', async (c) => {
-		const userId = c.get('userId');
-		const obAccountService = getService('obaccount');
+	.get("/accounts", async (c) => {
+		const userId = c.get("userId");
+		const obAccountService = getService("obaccount");
 
 		const accounts = await obAccountService.listAccounts(userId as string);
 
